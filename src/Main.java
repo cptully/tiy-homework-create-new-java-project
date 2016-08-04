04ed4ad0873f65f4909d60a98e1c54b79d3349c0
Chris Tully <cptully@gmail.com>
2016-08-04 10:12:27 -0400

    fix order of operations confusion and order of operation test

--- src/Main.java
+++ src/Main.java
@@ -9,32 +9,33 @@ public class Main {
         double d2 = 99.321;
 
         // integer addition
-        System.out.println("x1 plus x2: " + x1 + x2);
+        System.out.println("x1 plus x2: " + (x1 + x2));
 
         // integer subtraction; to print out the result of subtraction
         // this way the operation has to be in parenthesis!
         System.out.println("x1 minus x2: " + (x1 - x2));
+        System.out.println(x1 - x2 +"x1 minus x2: ");
 
         // integer multiplication
-        System.out.println("x1 times x2: " + x1 * x2);
+        System.out.println("x1 times x2: " + (x1 * x2));
 
         // integer division
-        System.out.println("x1 divide by x2: " + x1 / x2);
+        System.out.println("x1 divide by x2: " + (x1 / x2));
 
         // double addition
-        System.out.println("d1 plus d2: " + d1 + d2);
+        System.out.println("d1 plus d2: " + (d1 + d2));
 
         // double subtraction
         System.out.println("d1 minus d2: " + (d1 - d2));
 
         // double multiplication
-        System.out.println("d1 times d2: " + d1 * d2);
+        System.out.println("d1 times d2: " + (d1 * d2));
 
         // double division
-        System.out.println("d1 divide by d2: " + d1 / d2);
+        System.out.println("d1 divide by d2: " + (d1 / d2));
 
         // adding a double to an integer
-        System.out.println("x1 plus d2: " + x1 + d2);
+        System.out.println("x1 plus d2: " + (x1 + d2));
 
         // multiplying a double with an integer
         System.out.println("d1 times x2: " + d1 * x2);
